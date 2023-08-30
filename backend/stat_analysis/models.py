4cac66624e15a8da2a062997925fed4e7e295aac
Xys-Tus <Sixtus Onyedibe>
2023-08-30 21:45:30 +0100

    Added authentication and prisma schema

---
+++ backend/stat_analysis/models.py
@@ -0,0 +1,26 @@
+from stat_analysis import db, app
+
+# To create our models
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+   
+
+
+# ...
+
+# class Student(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     firstname = db.Column(db.String(100), nullable=False)
+#     lastname = db.Column(db.String(100), nullable=False)
+#     email = db.Column(db.String(80), unique=True, nullable=False)
+#     age = db.Column(db.Integer)
+#     created_at = db.Column(db.DateTime(timezone=True),
+#                            server_default=func.now())
+#     bio = db.Column(db.Text)
+
+#     def __repr__(self):
+#         return f'<Student {self.firstname}>'
