2ea1f847d5ae49cb12cec8090a6c3c63cf4e6a69
Xys-Tus <Sixtus Onyedibe>
2023-09-05 12:54:25 +0100

    made progress with posting and saving to the db. Also added profile page for the user saved datat

--- backend/stat_analysis/routes.py
+++ backend/stat_analysis/routes.py
@@ -1,11 +1,12 @@
 import os
+from io import BytesIO
 import csv
-# from flask import  request
 from werkzeug.utils import secure_filename
-from stat_analysis.models import User, Post
+from stat_analysis.models import User, Post, Upload
 from stat_analysis import app, db, CORS, cross_origin
-from flask import Flask, flash, request, redirect, url_for, session
+from flask import Flask, flash, request, redirect, url_for, session, send_file
 import logging
+import json
 
 
 
@@ -32,7 +33,7 @@ def fileUpload():
         os.makedirs(folder_path)
     file = request.files['file'] 
     logger.info("welcome to upload`")
-    file_path = os.path.join(folder_path,secure_filename(file.filename))
+    file_path = (os.path.join(folder_path,secure_filename(file.filename)))
     file.save(file_path) # Then save the file
     # Reading the file and parsing the files.
     fileContent = []
@@ -42,12 +43,33 @@ def fileUpload():
             fileContent.append(row)
     return fileContent
 
+
+@app.route('/postdata', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        file = request.files['file']
+        upload = Upload(filename=file.filename, data=file.read(), user_id=1)
+        db.session.add(upload)
+        db.session.commit()
+        return 'success'
+    upload = Upload.query.filter_by(user_id=1).all()
+    fileContents = []
+    for upload_data in upload:
+        lines = upload_data.data.decode('utf-8').split('\r\n')  # Decode bytes to a string and split into lines
+        csv_data = [line.split(',') for line in lines if line]  # Split each line by comma into a list of items
+        fileContents.append(csv_data)
+    return fileContents
+
+
+
 @app.route('/register', methods=['POST', 'GET'])
 def register():
-    if(len(file_path) > 0):
-        return file_path
+    # data = json.loads(request.get_json(force=True))
     data = request.get_json(force=True)
-        # user = User(username=data['name'], email=data['email'])
-        # db.session.add(user)
-        # db.session.commit()
+    print(data)
+    user = User(username=data['username'], email=data['email'])
+    db.session.add(user)
+    db.session.commit()
     return data
+
+
